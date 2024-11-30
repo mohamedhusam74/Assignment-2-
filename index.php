a3e492280272800425fccf40135874618f698172
mohamdarwish <mohamedhusam2003@gmail.com>
2024-11-30 21:21:05 +0300

    api url

---
+++ index.php
@@ -0,0 +1,2 @@
+<?php
+$apiUrl = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100";
